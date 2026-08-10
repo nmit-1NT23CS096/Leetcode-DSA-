19efe3b7de8f04c63d52d137e9ebf79ba4c2fbe6
K MALLIKARJUN <1nt23cs096.mallikarjun@nmit.ac.in>
2026-08-11 00:00:28 +0530

    Time: 12 ms (98.93%), Space: 59.3 MB (53.38%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -1,18 +1,15 @@
 class Solution {
     public int minSwaps(String s) {
-        Stack<Character> stack = new Stack();
-        int unbalanced = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-           
-            if (ch == '[') stack.push(ch);
-            else {
-                
-                if (!stack.isEmpty()) stack.pop();
-            
-                else unbalanced++;
+        int size = 0;
+
+        for(char ch : s.toCharArray()) {
+            if(ch == '[') {
+                size++;
+            } else if(size > 0) {
+                size--;
             }
         }
-        return (unbalanced + 1) / 2;
+
+        return (size + 1) / 2;
     }
 }
