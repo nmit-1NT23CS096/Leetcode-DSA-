e64f588b817068631ebebadb1c50048ef5efdfd5
K MALLIKARJUN <1nt23cs096.mallikarjun@nmit.ac.in>
2026-08-18 22:06:05 +0530

    Time: 0 ms (100%), Space: 43.1 MB (16.53%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -10,11 +10,37 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        ListNode slow = head, fast = head;
-        while(slow != null && fast != null && fast.next != null ){
-            fast =  fast.next.next;
-            slow= slow.next;
+             
+        if (head == null || head.next == null) {
+            return head;
         }
-        return slow;
+
+        ListNode temp = head;
+        int count = 0;
+
+        
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+
+        
+        int mid = count / 2 + 1;
+        temp = head;
+
+        while (temp != null) {
+            mid = mid - 1;
+
+            
+            if (mid == 0){
+               
+                break;
+            }
+            
+            temp = temp.next;
+        }
+
+        
+        return temp;
     }
 }
