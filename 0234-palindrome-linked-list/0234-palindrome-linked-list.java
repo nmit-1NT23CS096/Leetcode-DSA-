dbb87202e40272f02b4164bd079bd2a7ba46d012
K MALLIKARJUN <1nt23cs096.mallikarjun@nmit.ac.in>
2026-08-19 22:08:40 +0530

    Time: 8 ms (18.07%), Space: 96.4 MB (32.37%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,21 +9,38 @@
  * }
  */
 class Solution {
+    public ListNode reverseLL(ListNode head){
+        if(head == null || head.next ==null) return head;
+        ListNode newHead = reverseLL(head.next);
+        ListNode front = head.next;
+        front.next = head;
+        head.next = null;
+        return newHead;
+    }
     public boolean isPalindrome(ListNode head) {
-        Stack<Integer> stk= new Stack<>();
-        ListNode curr = head;
-        while(curr != null){
-            stk.push(curr.val);
-            curr = curr.next;
+
+        if(head == null || head.next ==null) return true;
+
+        ListNode slow = head, fast= head;
+        
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+             fast = fast.next.next;
         }
-        ListNode temp= head;
-        while(temp != null){
-            if(temp.val != stk.peek()){
+        ListNode newHead = reverseLL(slow.next);
+        ListNode first = head;
+        ListNode second = newHead;
+
+        while( second != null){
+            if(first.val != second.val){
                 return false;
             }
-            stk.pop();
-            temp = temp.next;
+            first = first.next;
+            second = second.next;
         }
+        reverseLL(newHead);
         return true;
+
     }
 }
