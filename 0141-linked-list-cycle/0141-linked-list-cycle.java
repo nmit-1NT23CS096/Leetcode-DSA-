91a8dbeaa39c5a1d6bec010c6cde9da837cea720
K MALLIKARJUN <1nt23cs096.mallikarjun@nmit.ac.in>
2026-09-12 16:12:57 +0530

    Time: 8 ms (8.94%), Space: 46.7 MB (52.84%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -10,16 +10,17 @@
  * }
  */
 public class Solution {
-    public boolean hasCycle(ListNode head){
-        HashMap<ListNode, Integer> map = new HashMap<>();
-        ListNode curr = head;
-        while(curr!=null){
-            if(map.containsKey(curr)){
+    public boolean hasCycle(ListNode head) {
+        ListNode  temp = head;
+        HashMap<ListNode,Integer> map= new HashMap<>();
+        while(temp != null){
+            if(map.containsKey(temp)){
                 return true;
             }
-            map.put(curr,1);
-            curr = curr.next;
+            map.put(temp,1);
+            temp = temp.next;
         }
         return false;
+
     }
 }
